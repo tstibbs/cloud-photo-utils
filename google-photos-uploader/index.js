6c0af939060ebcb3c7872b9ff31675076e2076a9
tstibbs <tstibbs@users.noreply.github.com>
2020-06-06 07:52:28 +0100

    Added google photos uploader

---
+++ google-photos-uploader/index.js
@@ -0,0 +1,148 @@
+require('dotenv').config()
+const google_client_id = process.env.google_client_id
+const google_project_id = process.env.google_project_id
+const google_client_secret = process.env.google_client_secret
+
+//=====================================================
+
+const fs = require('fs');
+const util = require('util');
+const {google} = require('googleapis');
+const request = require('request');
+const readFile = util.promisify(fs.readFile)
+
+const SCOPES = ['https://www.googleapis.com/auth/photoslibrary.appendonly'];
+
+const credentials = {
+	"installed": {
+		"client_id": google_client_id,
+		"project_id": google_project_id,
+		"auth_uri":"https://accounts.google.com/o/oauth2/auth",
+		"token_uri":"https://oauth2.googleapis.com/token",
+		"auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
+		"client_secret": google_client_secret,
+		"redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]
+	}
+};
+
+const authClient = new google.auth.OAuth2(credentials.installed.client_id, credentials.installed.client_secret, "http://localhost/success");
+
+if (!module.parent) { //i.e. if being invoked directly on the command line
+	let allArgs = process.argv.slice(2);
+	if (allArgs.length == 1 && allArgs[0] == 'login') {
+		console.log(buildAuthUrl());
+	} else if (allArgs.length == 2 && allArgs[0] == 'success') {
+		handleToken(allArgs[1]);
+	} else if (allArgs.length == 2 && allArgs[0] == 'upload') {
+		upload(allArgs[1]);
+	} else {
+		let command = `${process.argv[0]}`.split(/(\/|\\)/).slice(-1)[0]
+		let module = `${process.argv[1]}`.split(/(\/|\\)/).slice(-1)[0]
+		console.error(`Usage:
+    ${command} ${module} login
+    ${command} ${module} success code-from-url
+    ${command} ${module} upload file-path`)
+	}
+}
+
+function buildAuthUrl() {
+	//use the referrer to find the right origin and then pass the right redirect url. Possibly cache the initial client objects?
+	return authClient.generateAuthUrl({
+		scope: SCOPES
+	});
+}
+
+function writeAuth(tokens) {
+	fs.writeFile('tmp/auth.json', JSON.stringify(tokens, null, 2), (err) => {
+		if (err) {
+			console.error(err)
+			return
+		}
+	})
+}
+
+function handleToken(code) {
+	//probably doesn't matter if we use one configured with the right redirect url or not in this case
+	return authClient.getToken(code).then(res => {
+		return res.tokens;
+	}).then(tokens => {
+		console.log(tokens);
+		writeAuth(tokens);
+	}).catch(err => {
+		console.error(err);
+	});
+}
+
+async function upload(path) {
+	let contents = await readFile('tmp/auth.json')
+	let auth = JSON.parse(contents);
+	let expiryDate = new Date(auth.expiry_date);
+	expiryDate.setMinutes(expiryDate.getMinutes() - 5)//allow ourselves some time to get the upload done before this expires
+	if (expiryDate < Date.now()) {
+		console.log('refreshing token');
+		let data = await authClient.refreshToken(auth.refresh_token)
+		console.log(data.tokens);
+		auth.access_token = data.tokens.access_token;
+		auth.expiry_date = data.tokens.expiry_date;
+		writeAuth(auth);
+	}
+	
+	uploadContents(auth.access_token, path);
+}
+
+function uploadContents(oauthToken, fileName) {
+	let input = fs.createReadStream(fileName);
+	let output = request.post('https://photoslibrary.googleapis.com/v1/uploads', {
+		headers: {
+			'Content-type': 'application/octet-stream',
+			'Authorization': `Bearer ${oauthToken}`,
+			'X-Goog-Upload-File-Name': fileName,
+			'X-Goog-Upload-Protocol': 'raw'
+		}
+	}, (error, response, uploadToken) => {
+		if (!error && response.statusCode == 200) {
+			console.log(uploadToken);
+			registerUpload(oauthToken, uploadToken, fileName);
+			//response should be an upload token
+		} else {
+			console.error(error);
+			console.error(response);
+			console.error(uploadToken);
+		}
+	});
+	input.pipe(output);
+}
+
+function registerUpload(oauthToken, uploadToken, fileName) {
+	let body = JSON.stringify({
+		//"albumId": "",//doesn't work unless this 'app' (i.e. these oauth credentials) created the album
+		"newMediaItems": [
+			{
+				"description": fileName,
+				"simpleMediaItem": {
+					"uploadToken": uploadToken
+				}
+			}
+		]
+	}, null, 2);
+	let output = request.post('https://photoslibrary.googleapis.com/v1/mediaItems:batchCreate', {
+		headers: {
+			'Content-type': 'application/json',
+			'Authorization': `Bearer ${oauthToken}`
+		},
+		body: body
+	}, (error, response, body) => {
+		if (!error && response.statusCode == 200) {
+			console.log(body);
+			//response should be an upload token
+		} else {
+			console.error(error);
+			console.error(response);
+			console.error(body);
+		}
+	});
+}
+
+module.exports = {
+	upload
+}
