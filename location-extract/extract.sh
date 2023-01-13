0bbc91f235224a85d2a6df6736cb6925b1c6990e
Tim Stibbs <tstibbs@users.noreply.github.com>
2023-01-13 11:39:36 +0000

    Added script to extract geo location from files

---
+++ location-extract/extract.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+if [[ $# -ne 2 ]]
+then
+    echo "Usage: extract.sh dir outputfile"
+	exit 1
+fi
+
+dir=$1
+outputFile=$2
+
+exiftool -filename -directory -gpslatitude -gpslongitude -ext JPG -ext JPEG -table --printConv -recurse . | grep -v -P '\t\-\t\-$' | awk '{ print $2 "/" $1 "," $3 "," $4 }' > $outputFile
